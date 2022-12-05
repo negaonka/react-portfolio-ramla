5771396cdb2b46bf87fb34bd722baafc1672700d
ramlaahmed <ramlaahmed01@gmail.com>
2022-12-05 22:46:05 +0000

    added another card

--- src/projects.js
+++ src/projects.js
@@ -1,47 +1,65 @@
 const projects = [
     {
         id: 1,
-        name: "Weather Dashboard",
-        description: "In this project, i created a weather dashboard that displayed the current weather conditions in that city. ",
-        image: require('./assets/weatherdash.png'),
-        siteAddress: "https://ramlaahmed.github.io/weather-dashboard/",
-        gitHub: "https://github.com/ramlaahmed/weather-dashboard",
+        name: "Marketing Agency",
+        description: "This is the first project i had to do as a homework. For this project, i had to refactor existing code.",
+        image: require('./assets/hori.png'),
+        siteURL: "hhttps://ramlaahmed.github.io/marketing-agency//",
+        github: "https://github.com/ramlaahmed/marketing-agency",
         technologiesUsed:["HTML",'CSS', 'API', 'JavaScript']
     },
     {
         id: 2,
-        name: "Password Generator",
-        description: "In this project, i created a generate the password that meets certain criteria.",
-        image: require('./assets/passwordgen.png'),
-        siteAddress: "https://ramlaahmed.github.io/password-generatorr",
-        gitHub: "https://github.com/ramlaahmed/password-generator",
-        technologiesUsed:["HTML",'CSS','JavaScript']
-    },
-    {
-        id:3,
-        name: "E-commerce-Back-End",
-        description: "In this project, i created a mysql database and backend application for an e-commence site.",
-        image: require('./assets/ecommerce.png'),
-        siteAddress: 'https://drive.google.com/file/d/1iBQuqCTNbI8ZOHhTefSPKUwmyWNBA1Oh/view',
-        gitHub: "https://github.com/ramlaahmed/E-commerce-Back-End",
+        name: "Weather Dashboard",
+        description: "In this project, i created a weather dashboard that displayed the current weather conditions in that city. ",
+        image: require('./assets/weatherdash.png'),
+        siteURL: "https://ramlaahmed.github.io/weather-dashboard/",
+        github: "https://github.com/ramlaahmed/weather-dashboard",
         technologiesUsed:["HTML",'CSS', 'API', 'JavaScript']
     },
     {
-    id:4,
+        id: 3,
+        name: "Password Generator",
+        description: "In this project, i created an application that generates a password.",
+        image: require('./assets/passwordgen.png'),
+        siteURL: "https://ramlaahmed.github.io/password-generatorr",
+        github: "https://github.com/ramlaahmed/password-generator",
+        technologiesUsed:["HTML",'CSS','JavaScript']
+    },
+    {
+        id:4,
+        name: "E-commerce-Back-End",
+        description: "In this project, i created a mysql database and backend application for an e-commence site.",
+        image: require('./assets/ecommerce.png'),
+        siteURL: 'https://drive.google.com/file/d/1iBQuqCTNbI8ZOHhTefSPKUwmyWNBA1Oh/view',
+        github: "https://github.com/ramlaahmed/E-commerce-Back-End",
+        technologiesUsed:["HTML",'CSS', 'API', 'JavaScript']
+    },
+    {
+    id:5,
     name: "Work Day Schedule",
     description: "In this project, i created an efficient calender app for scheduling an individuals work day.",
     image: require('./assets/workdayscheduler.png'),
-    siteAddress: 'https://ramlaahmed.github.io/workdayschedule/',
-    gitHub: "https://github.com/ramlaahmed/workdayschedule",
+    github: 'https://ramlaahmed.github.io/workdayschedule/',
+    github: "https://github.com/ramlaahmed/workdayschedule",
     technologiesUsed:["HTML",'CSS', 'MOMENT.JS', 'JavaScript','JQUERY']
    },
+   {
+    id:6,
+    name: "Note Taker",
+    description: "In this project, i had to create an application called note taker that can be used to to write and save notes.",
+    image: require('./assets/notetaker.png'),
+    github: 'https://evening-sierra-44458.herokuapp.com/',
+    github: "https://github.com/ramlaahmed/note-taker",
+    technologiesUsed:["HTML",'CSS','JavaScript']
+   },
      {
-        id: 5,
+        id: 7,
         name: "Show me more",
         description: "All of my other Projects",
         image:require('./assets/github.png') ,
-        siteAddress: "https://ramlaahmed.github.io/show-me-more/",
-        gitHub: "https://github.com/ramlaahmed",
+        siteURL: "https://ramlaahmed.github.io/show-me-more/",
+        github: "https://github.com/ramlaahmed",
    }
 ]
 
