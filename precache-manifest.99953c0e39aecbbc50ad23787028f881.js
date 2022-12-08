4c201f1431ae3611809cd715100d68381f930635
negaonka <neha.n.gaonkar@gmail.com>
2022-12-08 15:03:50 +0000

    Updates

---
+++ precache-manifest.99953c0e39aecbbc50ad23787028f881.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "7221d4a6308049c60120",
+    "url": "/react-portfolio-ramla/static/css/main.e53d7817.chunk.css"
+  },
+  {
+    "revision": "7221d4a6308049c60120",
+    "url": "/react-portfolio-ramla/static/js/main.7221d4a6.chunk.js"
+  },
+  {
+    "revision": "9b41d909b2f9cc95d618",
+    "url": "/react-portfolio-ramla/static/js/1.9b41d909.chunk.js"
+  },
+  {
+    "revision": "5d19638f056dbca0c40e",
+    "url": "/react-portfolio-ramla/static/js/runtime~main.5d19638f.js"
+  },
+  {
+    "revision": "2059a0b257b7a2f4b024a04d7f988125",
+    "url": "/react-portfolio-ramla/static/media/defaultImage.2059a0b2.jpeg"
+  },
+  {
+    "revision": "2523e37546ef898b8d3970877d3b8bc3",
+    "url": "/react-portfolio-ramla/static/media/hori.2523e375.png"
+  },
+  {
+    "revision": "47d9f219abb1fdeb9f49cf0f1d8fa3c9",
+    "url": "/react-portfolio-ramla/static/media/weatherdash.47d9f219.png"
+  },
+  {
+    "revision": "14c77815fe8f913add0490f55abc5d04",
+    "url": "/react-portfolio-ramla/static/media/passwordgen.14c77815.png"
+  },
+  {
+    "revision": "535427a4470d5e40b1166dad6ed8d2cf",
+    "url": "/react-portfolio-ramla/static/media/ecommerce.535427a4.png"
+  },
+  {
+    "revision": "eabcb3621987005b4b260657838eaf91",
+    "url": "/react-portfolio-ramla/static/media/workdayscheduler.eabcb362.png"
+  },
+  {
+    "revision": "356915d2e7f8bbc6eb66be0996f0d571",
+    "url": "/react-portfolio-ramla/static/media/notetaker.356915d2.png"
+  },
+  {
+    "revision": "c032e01c74733de3fd2baf90199a0b53",
+    "url": "/react-portfolio-ramla/static/media/github.c032e01c.png"
+  },
+  {
+    "revision": "ee144e119ff42dd8a9a278b7c4a82554",
+    "url": "/react-portfolio-ramla/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8cf14e977275c558bfc0e0525e65e88.js"
+  "/react-portfolio-ramla/precache-manifest.99953c0e39aecbbc50ad23787028f881.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-portfolio-ramla/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
