bac1027fcdcb54e7ff91067213807b9ec0cc81ad
negaonka <neha.n.gaonkar@gmail.com>
2022-12-08 14:20:07 +0000

    Updates

---
+++ precache-manifest.c72242fff599864404b8dff0622d8d6e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bc1ca8a1ba2902ad917",
+    "url": "/static/css/main.5dc09c31.chunk.css"
+  },
+  {
+    "revision": "8bc1ca8a1ba2902ad917",
+    "url": "/static/js/main.8bc1ca8a.chunk.js"
+  },
+  {
+    "revision": "f12f5854ce980c3a420c",
+    "url": "/static/js/1.f12f5854.chunk.js"
+  },
+  {
+    "revision": "8efd90ff552e1bac6b87",
+    "url": "/static/css/2.45b93a33.chunk.css"
+  },
+  {
+    "revision": "8efd90ff552e1bac6b87",
+    "url": "/static/js/2.8efd90ff.chunk.js"
+  },
+  {
+    "revision": "86af42aa9a6f3e84c441",
+    "url": "/static/js/runtime~main.86af42aa.js"
+  },
+  {
+    "revision": "2059a0b257b7a2f4b024a04d7f988125",
+    "url": "/static/media/defaultImage.2059a0b2.jpeg"
+  },
+  {
+    "revision": "2523e37546ef898b8d3970877d3b8bc3",
+    "url": "/static/media/hori.2523e375.png"
+  },
+  {
+    "revision": "47d9f219abb1fdeb9f49cf0f1d8fa3c9",
+    "url": "/static/media/weatherdash.47d9f219.png"
+  },
+  {
+    "revision": "14c77815fe8f913add0490f55abc5d04",
+    "url": "/static/media/passwordgen.14c77815.png"
+  },
+  {
+    "revision": "535427a4470d5e40b1166dad6ed8d2cf",
+    "url": "/static/media/ecommerce.535427a4.png"
+  },
+  {
+    "revision": "eabcb3621987005b4b260657838eaf91",
+    "url": "/static/media/workdayscheduler.eabcb362.png"
+  },
+  {
+    "revision": "356915d2e7f8bbc6eb66be0996f0d571",
+    "url": "/static/media/notetaker.356915d2.png"
+  },
+  {
+    "revision": "c032e01c74733de3fd2baf90199a0b53",
+    "url": "/static/media/github.c032e01c.png"
+  },
+  {
+    "revision": "77cc09341e4e0e664803c912f1dcd779",
+    "url": "/index.html"
+  }
+];
